c30d481d07faccc0c2d49878022ce7f70961c97e
Ankit9918 <sahuankit9918@gmail.com>
2025-04-10 23:59:15 +0530

    Port Problem

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { configDotenv } from "dotenv"
+
+configDotenv();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/",(req,res)=>{
+    res.send("Anshul");
+})
+
+app.listen(port);
