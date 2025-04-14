da1d17d63c4c7afc386c0bda813964822601be47
AmitPandey-AP <pandeyamit5969@gmail.com>
2025-04-14 22:38:58 +0530

    frontend  Url adding

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB();
 await connectCloudinary();
 
 // allow multiple origins
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173","https://groww-carry-frontend.vercel.app"];
 
 // stripe webhook thing -> for verification of payment
 app.post(
